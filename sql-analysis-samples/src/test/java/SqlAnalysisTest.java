3d75351d4cd17ff1531d4491be2356ee5e32b52d
Breno A. <git@breno.tech>
2024-05-01 13:57:30 -0300

    tests: add some new tests

--- sql-analysis-samples/src/test/java/SqlAnalysisTest.java
+++ sql-analysis-samples/src/test/java/SqlAnalysisTest.java
@@ -1,10 +1,16 @@
+import com.jd.sql.analysis.analysis.SqlAnalysis;
+import com.jd.sql.analysis.extract.SqlExtractResult;
 import com.jd.sql.analysis_samples.mapper.TaskMapper;
 import com.jd.sql.analysis_samples.po.Task;
 import com.jd.sql.analysis_samples.util.MyBatisUtil;
 import org.apache.ibatis.session.SqlSession;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
+
+import static org.junit.Assert.assertNull;
 
 /**
  * @Author huhaitao21
@@ -13,27 +19,51 @@ import java.util.List;
  **/
 public class SqlAnalysisTest {
 
-    private TaskMapper mapper;
+		private TaskMapper mapper;
+		private SqlSession sqlSession;
 
-    @Test
-    public void testFastSql(){
-        //走索引
-        SqlSession  sqlSession = MyBatisUtil.getSqlSession();
-        mapper = sqlSession.getMapper(TaskMapper.class);
-        Task task = new Task();
-        task.setCreateUser("zhangsan1");
-        List<Task> list = mapper.queryAll(task);
+		@Before
+		public void setUp() {
+				sqlSession = MyBatisUtil.getSqlSession();
+				mapper = sqlSession.getMapper(TaskMapper.class);
+		}
 
-    }
+		@Test
+		public void testAnalysisWithNullConnection() {
+				SqlExtractResult sqlExtractResult = new SqlExtractResult();
+				sqlExtractResult.setSourceSql("SELECT * FROM task");
+				assertNull(SqlAnalysis.analysis(sqlExtractResult, null));
+		}
 
-    @Test
-    public void testSimpleSlowSql(){
-        //不走索引
-        SqlSession  sqlSession = MyBatisUtil.getSqlSession();
-        mapper = sqlSession.getMapper(TaskMapper.class);
-        Task task = new Task();
-        task.setTitle("测试");
-        List<Task> list = mapper.queryAll(task);
-    }
+		@Test
+		public void testFastSql() {
+				Task task = new Task();
+				task.setCreateUser("zhangsan1");
+				List<Task> list = mapper.queryAll(task);
+				Assert.assertNotNull(list);
 
+		}
+
+		@Test
+		public void testSimpleSlowSql() {
+				Task task = new Task();
+				task.setTitle("测试");
+				List<Task> list = mapper.queryAll(task);
+				Assert.assertNotNull(list);
+		}
+
+
+		@Test
+		public void testNewSql1() {
+				SqlExtractResult sqlExtractResult = new SqlExtractResult();
+				sqlExtractResult.setSourceSql("SELECT * FROM task WHERE create_user = 'zhangsan1'");
+				Assert.assertNotNull(SqlAnalysis.analysis(sqlExtractResult, sqlSession.getConnection()));
+		}
+
+		@Test
+		public void testNewSql2() {
+				SqlExtractResult sqlExtractResult = new SqlExtractResult();
+				sqlExtractResult.setSourceSql("SELECT * FROM task WHERE title = '测试'");
+				Assert.assertNotNull(SqlAnalysis.analysis(sqlExtractResult, sqlSession.getConnection()));
+		}
 }
