c636bc3a0b770f14bd7b0456244e38a295027cf0
huhaitao21 <huhaitao21@jd.com>
2024-05-16 21:42:15 +0800

    补充单测

--- sql-analysis-samples/src/test/java/SqlAnalysisTest.java
+++ sql-analysis-samples/src/test/java/SqlAnalysisTest.java
@@ -1,9 +1,11 @@
+import com.jd.sql.analysis.util.GsonUtil;
 import com.jd.sql.analysis_samples.mapper.TaskMapper;
 import com.jd.sql.analysis_samples.po.Task;
 import com.jd.sql.analysis_samples.util.MyBatisUtil;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
@@ -34,6 +36,43 @@ public class SqlAnalysisTest {
         Task task = new Task();
         task.setTitle("测试");
         List<Task> list = mapper.queryAll(task);
+    }
+
+    @Test
+    public void testSimpleUpdateSql(){
+        SqlSession  sqlSession = MyBatisUtil.getSqlSession();
+        mapper = sqlSession.getMapper(TaskMapper.class);
+
+        Task task = new Task();
+        task.setContent("测试内容");
+        task.setTitle("测试");
+        task.setId(1l);
+        mapper.updateByPrimaryKey(task);
+    }
+    @Test
+    public void testSqlReplace(){
+        SqlSession  sqlSession = MyBatisUtil.getSqlSession();
+        mapper = sqlSession.getMapper(TaskMapper.class);
+
+    }
+    @Test
+    public void testManyParaSql(){
+        SqlSession  sqlSession = MyBatisUtil.getSqlSession();
+        mapper = sqlSession.getMapper(TaskMapper.class);
+        Task task = new Task();
+        task.setTitle("测试");
+        List<Task> list = mapper.queryAllM(task,1l);
+        System.out.println(GsonUtil.bean2Json(list));
+    }
+
+    @Test
+    public void testBindSql(){
+        SqlSession  sqlSession = MyBatisUtil.getSqlSession();
+        mapper = sqlSession.getMapper(TaskMapper.class);
+        Task task = new Task();
+        task.setTitle("测试");
+        List<Task> list = mapper.queryLike(task);
+        System.out.println(GsonUtil.bean2Json(list));
     }
 
 }
